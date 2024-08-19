cf136ce9a914ea35ee2c66ed4d38cb64c03e3fa6
Yurii Soroka <urasor04@gmail.com>
2024-08-20 02:20:51 +0300

    Minor code changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
-const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button');
-
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
 
 const sendHttpRequest = (method, url, data) => {
   const promise = new Promise((resolve, reject) => {
@@ -20,7 +19,7 @@ const sendHttpRequest = (method, url, data) => {
 };
 
 async function fetchPosts() {
-  listElement.innerHTML = '';
+  listElement.innerHTML = "";
   const responseData = await sendHttpRequest(
     "GET",
     "https://jsonplaceholder.typicode.com/posts"
@@ -35,21 +34,19 @@ async function fetchPosts() {
 }
 
 async function createPost(title, content) {
-    const userId = Math.random();
-    const post = {
-        title: title,
-        body: content,
-        userId: userId
-    };
-    sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
-
-    
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
 }
 
-fetchButton.addEventListener('click', fetchPosts);
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const enteredTitle = event.currentTarget.querySelector('#title').value;
-    const enteredContent = event.currentTarget.querySelector('#content').value;
-    createPost(enteredTitle, enteredContent);
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+  createPost(enteredTitle, enteredContent);
 });
