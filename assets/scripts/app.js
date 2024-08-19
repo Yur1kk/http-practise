908564eb5a4fc9f3821d849adea1936bcd5153ca
Yurii Soroka <urasor04@gmail.com>
2024-08-20 01:37:59 +0300

    Provided parsing json objects to JS objects, provided adding them on the webpage

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,20 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
-xhr.onload =function() {
-    
+xhr.onload = function() {
+   const listOfPost = JSON.parse(xhr.response);
+   for (const post of listOfPost) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
 }
+};
 
 
 xhr.send();
+
