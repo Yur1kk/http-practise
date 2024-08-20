e3778f11961f730c9a7344cc88b5102d0739a193
Yurii Soroka <urasor04@gmail.com>
2024-08-20 03:41:05 +0300

    Added new header

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,9 @@ const sendHttpRequest = (method, url, data) => {
   return fetch(url, {
     method: method,
     body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
   }).then((response) => {
     return response.json();
   });
